fb6f32cb45352ab5b1854b98cdc30dde0e25c8e6
rupeshcash <rupesh21kash@gmail.com>
2024-01-03 23:37:42 +0530

    refactored code + ui minor changes

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ import Table from './components/PredictionTable';
 function App() {
   const [searchResult, setSearchResult] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
-  
+  const [searchedCity, setSearchedCity] = useState('');
+
   const handleSearch = (query) => {
-    // Make API request when the user clicks "Search"
+    
     fetch(`http://localhost:8080/api/v1/weather?city=${query}`)
       .then(response => response.json())
       .then(data => {
         if (data.code === 200) {
-          setSearchResult(data.data); // Update state with the search result
+          setSearchResult(data.data); 
           setErrorMessage('');
+          setSearchedCity(query);
         } else {
-          setErrorMessage(data.message || 'An error occurred.');
+          setErrorMessage(data.data.errorDescription || 'An error occurred.');
         }
       })
       .catch(error => {
         console.error('Error fetching data:', error);
         setErrorMessage('An error occurred.');
-        // Handle error appropriately (e.g., show an error message)
       });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, World!</h1>
+        <h1>Weather Predictor!</h1>
         <div style={{ textAlign: 'center' }}>
           <SearchBar onSearch={handleSearch} />
         </div>
+        {searchedCity && <p>Searched City: {searchedCity}</p>}
         {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <Table data={searchResult} />
       </header>
